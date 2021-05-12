885af6e46eb1bf10683b94c9d80bfd35934ee89c
aguerosys <aguerolinkk@gmail.com>
2021-05-11 21:37:20 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## About Api-New
+
+is an api where you learn to use TDD with laravel
+
---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use App\Http\Requests\Post as PostRequests; //as nos permite darle un alias
+
+class PostController extends Controller
+{
+    protected $post;
+
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+    }
+
+    public function index()
+    {
+        //
+        //return dd($this->post->paginate());
+        return response()->json($this->post->paginate());
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PostRequests $request)
+    {
+        //
+
+        $post = $this->post->create($request->all());
+
+        return response()->json($post, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        //
+        return response()->json($post);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PostRequests $request, Post $post)
+    {
+        //
+        $post->update($request->all());
+        return response()->json($post);
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //
+
+        $post->delete();
+
+        return response()->json(null, 204);
+    }
+}
